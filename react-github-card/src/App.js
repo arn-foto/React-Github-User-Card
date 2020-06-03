789191c70bece15bbfbf62376e874d74743eb5ef
Anthony N <foto.arn@gmail.com>
2020-06-02 21:44:13 -0700

    created axios get request + user/follower card files

--- react-github-card/src/App.js
+++ react-github-card/src/App.js
@@ -1,14 +1,56 @@
 import React from 'react';
-import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import Usercard from "./Componenets/usercard";
+import Followercard from "./Componenets/followerCard";
 
-function App() {
-  return (
-    <div className="App">
-    <h2>GITHUB user card</h2>
-    </div>
-  );
+
+class App extends React.Component {
+    state= {
+      userData: [],
+      followersData: []
+  }
+
+
+  componentDidMount() {
+    axios
+    .get("https://api.github.com/users/arn-foto").then(response => {
+      this.setState({
+        userData: response.data
+      });
+      console.log(response.data)
+    })
+    
+    .catch(error => console.log(error));
+
+    axios
+    .get("https://api.github.com/users/arn-foto/followers")
+    .then(response => {
+      this.setState({
+        followersData: response.data
+      });
+      console.log(response.data)
+    })
+  }
+
+  
+
+  render() {
+    return (
+      <div className="App">
+          <h1>GitHub Usercard</h1>
+            <div className="alldata">
+              <Usercard userData={this.state.userData} className="user"></Usercard>
+              <Followercard 
+              followersData={this.state.followersData}
+              >
+              </Followercard>
+          </div>
+        <main />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ react-github-card/src/Componenets/usercard.js
@@ -0,0 +1,23 @@
+  
+import React from "react";
+
+
+export default class UserCard extends React.Component {
+    
+    render(){
+        return(
+            <div className="card">
+                <div className="data">
+                    <img src={this.props.userData.avatar_url}/>
+                    <p className="login">Name: {this.props.userData.login}</p>
+                    <p>URL: {this.props.userData.html_url}</p>
+                    <p>Bio: {this.props.userData.bio}</p>
+                    <p>Followers: {this.props.userData.followers}</p>
+                    <p>Following: {this.props.userData.following}</p>
+                    
+                </div>
+                <img src={`http://ghchart.rshah.org/${this.props.userData.login}`} alt="Github chart" className="chart"/>
+            </div>
+        )
+    }
+}
