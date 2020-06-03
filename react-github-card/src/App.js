9f5fb34a7ffc7853dbfb81062c2a0d7ac41ae022
Anthony N <foto.arn@gmail.com>
2020-06-02 21:28:46 -0700

    first commit

---
+++ react-github-card/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import axios from 'axios';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h2>GITHUB user card</h2>
+    </div>
+  );
+}
+
+export default App;
